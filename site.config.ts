b296195f4a8960d415bf206838f805663fcae516
LLLBK <76717329+LLLBK@users.noreply.github.com>
2022-09-03 16:45:11 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'b37ea7b6c1f747f5aa8b1b6595233d70?v=cae6a1af9cd34216a1c3b354dbf495c2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Bozhong Test',
+  domain: 'https://www.notion.so/b37ea7b6c1f747f5aa8b1b6595233d70?v=cae6a1af9cd34216a1c3b354dbf495c2',
+  author: 'Bozhong Liu',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'bozhong_liu',
+  github: 'LLLBK',
+  // linkedin: 'fisch2',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
